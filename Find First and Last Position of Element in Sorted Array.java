226fab54b4d5a19be6acbe985fad5b7ed261eac6
Abilash <abilash2809@gmail.com>
2025-03-22 22:40:52 +0530

    Create Find First and Last Position of Element in Sorted Array.java

---
+++ Find First and Last Position of Element in Sorted Array.java
@@ -0,0 +1,53 @@
+/*
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+*/
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target,true);
+        int last = findFirst(nums, target,false);
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target,boolean isleft) {
+        int left = 0, right = nums.length - 1;
+        int index = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                index = mid;
+                if(isleft){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    
+}
