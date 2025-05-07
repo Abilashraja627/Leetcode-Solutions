cb1582da0b7e6c2ed4baf39b7292f536dda58929
Abilash <abilash2809@gmail.com>
2025-05-07 22:37:27 +0530

    Create disappear.java

---
+++ disappear.java
@@ -0,0 +1,36 @@
+/*Find All Numbers Disappeared in an Array
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]*/
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int s=1;
+        HashSet<Integer>set=new HashSet<>();
+        for(int n:nums){
+            set.add(n);
+        }
+        List<Integer>list=new ArrayList<>();
+       while(s<=nums.length){
+        if(!set.contains(s)){
+            list.add(s);
+        }
+        s++;
+       }
+        return list;
+       
+    }
+}
