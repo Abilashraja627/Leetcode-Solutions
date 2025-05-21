1bc88cd05b58d75e301afbb8b06c97e66fd1de2d
Abilash <abilash2809@gmail.com>
2025-05-21 13:30:12 +0530

    Create setzeromatrix.java

---
+++ setzeromatrix.java
@@ -0,0 +1,52 @@
+/*Set Matrix Zeroes
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]*/
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+
+        // Step 1: Identify rows and columns that should be set to zero
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Step 2: Set rows to zero
+        for (int i = 0; i < n; i++) {
+            if (row[i]) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: Set columns to zero
+        for (int j = 0; j < m; j++) {
+            if (col[j]) {
+                for (int i = 0; i < n; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
